5a28642606a43530e2497c47cfe0bf1c12f83bd2
yd2058 <yd2058@bs.amalnet.k12.il>
2023-02-18 12:27:56 +0200

    update

--- app/src/main/java/com/example/game1/MainActivity.java
+++ app/src/main/java/com/example/game1/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Random rnd = new Random();
     int sum = 0;
     int temp1, temp2;
+    String str ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,23 +48,64 @@ public class MainActivity extends AppCompatActivity {
 
         temp1 = (rnd.nextInt(90)+10);
         temp2 = (rnd.nextInt(90)+10);
-        tv[0].setText(temp1);
-        tv[1].setText(temp2);
+        tv[0].setText(String.valueOf(temp1));
+        tv[1].setText(String.valueOf(temp2));
     }
 
     public void go1(View view) {
         if (!bo[0]){
-            if((temp1+temp2==et[0].getText()))
+            if((temp1+temp2==Integer.parseInt(String.valueOf(et[0].getText())))){
+                imgv[0].setImageResource(R.drawable.vee);sum++;}
+            else{
+                imgv[0].setImageResource(R.drawable.ix);}
+            bo[0] = true;
+            temp1 = (rnd.nextInt(90)+10);
+            temp2 = (rnd.nextInt(90)+10);
+            tv[2].setText(String.valueOf(temp1));
+            tv[3].setText(String.valueOf(temp2));
         }
     }
 
     public void go2(View view) {
+        if(bo[0]&&!(bo[1])){
+            if((temp1+temp2==Integer.parseInt(String.valueOf(et[1].getText())))){
+                imgv[1].setImageResource(R.drawable.vee); sum++;}
+            else{
+                imgv[1].setImageResource(R.drawable.ix);}
+            bo[1] = true;
+            temp1 = (rnd.nextInt(90)+10);
+            temp2 = (rnd.nextInt(90)+10);
+            tv[4].setText(String.valueOf(temp1));
+            tv[5].setText(String.valueOf(temp2));
+        }
     }
 
     public void go3(View view) {
+        if(bo[1]&&!(bo[2])){
+            if((temp1+temp2==Integer.parseInt(String.valueOf(et[2].getText())))){
+                imgv[2].setImageResource(R.drawable.vee); sum++;}
+            else{
+                imgv[2].setImageResource(R.drawable.ix);}
+            bo[2] = true;
+            str+= String.valueOf(100/sum) + " % done, " +String.valueOf(sum) + "/3";
+            Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 
-    public void clr(View view) {
 
-    }
 }
+
+    public void clrng(View view) {
+        for(int i = 0; i<3; i++)
+            bo[i] = false;
+        for(int i=0; i<6; i++)
+            tv[i].setText("");
+        imgv[0].setImageResource(R.drawable.trans);
+        imgv[1].setImageResource(R.drawable.trans);
+        imgv[2].setImageResource(R.drawable.trans);
+        str = "";
+        temp1 = (rnd.nextInt(90)+10);
+        temp2 = (rnd.nextInt(90)+10);
+        tv[0].setText(String.valueOf(temp1));
+        tv[1].setText(String.valueOf(temp2));
+    }
+    }
